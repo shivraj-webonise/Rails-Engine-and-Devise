3dbc65093b96675a9b57567414553f80a52b2fc3
shivraj <shivaraj.sayagaon@weboniselab.com>
2021-07-07 15:57:12 +0530

    testing of blog

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, length: {minimum: 2}
+    validates_presence_of :author
+    validates_presence_of :content
+    validates_presence_of :image_url
+
     belongs_to :user
 end
---
+++ blog/spec/request/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+    describe "/users/signup" do
+        it "succeds" do
+            get new_user_registration_path  
+            expect(response).to be_successful
+        end
+    end
+    describe "/users/signup" do
+        it "succeds" do
+            get new_user_session_path  
+            expect(response).to be_successful
+        end
+    end
+end
